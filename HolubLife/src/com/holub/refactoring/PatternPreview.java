8b12c7df63f231a5f0aec1a0ebb13889db9b3ea9
foba1 <foba1@naver.com>
2022-12-03 19:39:36 +0900

    Add patterns.
    Add patterns.

--- HolubLife/src/com/holub/refactoring/PatternPreview.java
+++ HolubLife/src/com/holub/refactoring/PatternPreview.java
@@ -14,7 +14,15 @@ public class PatternPreview {
 	private Boolean[][] curCellBoard;
 	private Boolean[][][] pattern = {
 			{{true, true}, {true, true}},
-			{{false, true, false}, {false, false, true}, {true, true, true}}
+			{{false, true, true, false}, {true, false, false, true}, {false, true, true, false}},
+			{{true, true, false}, {true, false, true}, {false, true, false}},
+			{{false, true, false}, {true, false, true}, {false, true, false}},
+			{{true, true, true}},
+			{{false, true, true, true}, {true, true, true, false}},
+			{{true, true, false, false}, {true, true, false, false}, {false, false, true, true}, {false, false, true, true}},
+			{{false, true, false}, {false, false, true}, {true, true, true}},
+			{{false, true, true, true, true}, {true, false, false, false, true}, {false, false, false, false, true}, {true, false, false, true, false}},
+			{{false, true, true, true, true, true, true}, {true, false, false, false, false, false, true}, {false, false, false, false, false, false, true}, {true, false, false, false, false, true, false}, {false, false, true, true, false, false, false}}
 	};
 	
 	private PatternPreview()
