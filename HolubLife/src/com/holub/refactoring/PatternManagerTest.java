96bef956edb8b54ee1a3317581cd58af24b0e269
foba1 <foba1@naver.com>
2022-12-09 23:02:58 +0900

    Add PatternManagerTest.
    Add PatternManagerTest.

---
+++ HolubLife/src/com/holub/refactoring/PatternManagerTest.java
@@ -0,0 +1,14 @@
+package com.holub.refactoring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PatternManagerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
