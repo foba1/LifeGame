795ecf34122f367578ab7118a7db6ef02d2354e2
foba1 <foba1@naver.com>
2022-12-10 00:12:41 +0900

    Add initialize, clearcellboard, selecttest, showtest.
    Add initialize, clearcellboard, selecttest, showtest.

--- HolubLife/src/com/holub/refactoring/PatternManagerTest.java
+++ HolubLife/src/com/holub/refactoring/PatternManagerTest.java
@@ -2,13 +2,81 @@ package com.holub.refactoring;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.BorderLayout;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PatternManagerTest {
 
+	@BeforeAll
+	public static void Initialize() {
+		Life.main(null);
+	}
+	
+	@BeforeEach
+	void ClearCellBoard() {
+		try {
+			Field field = Universe.instance().getClass().getDeclaredField("outermostCell");
+			field.setAccessible(true);
+			CellBoard board = (CellBoard)field.get(Universe.instance());
+			board.clear();
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void SelectTest() {
+		try {
+			Method method = PatternManager.instance().getClass().getDeclaredMethod("SelectPattern", int.class);
+			method.setAccessible(true);
+			method.invoke(PatternManager.instance(), 2);
+			
+			Field field = PatternManager.instance().getClass().getDeclaredField("curPattern");
+			field.setAccessible(true);
+			int value = (int)field.get(PatternManager.instance());
+			assertEquals(2, value);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void ShowTest() {
+		Boolean[][] beforeCellBoard = Universe.instance().getCellBoard();
+		
+		PatternManager.instance().Show(32, 32);
+		
+		Boolean[][] afterCellBoard = Universe.instance().getCellBoard();
+		
+		assertTrue(IsSameCellBoard(beforeCellBoard, afterCellBoard));
+	}
+	
+	private boolean IsSameCellBoard(Boolean[][] array1, Boolean[][] array2) {
+		for (int i = 0; i < array1.length; i++) {
+			for (int j = 0; j < array1.length; j++) {
+				if (array1[i][j] == array2[i][j]) continue;
+				else return false;
+			}
+		}
+		return true;
 	}
 
 }
